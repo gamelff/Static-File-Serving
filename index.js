6e581247b228ae85738ec091183aa0e991004bfd
Zach Freethy <zjfreethy@gmail.com>
2024-01-05 12:24:50 -0600

    Signed-off-by: Zach Freethy <zjfreethy@gmail.com>

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 const express = require('express')
 const path = require('path');
+const url = require('url');
 const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', async (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'desktop.html'));
+});
+
+app.listen(8080, () => {
+    console.log("Server successfully running on port 8080");
+});
